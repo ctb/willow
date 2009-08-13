eb78a0e298bc7aa5e173cbb015fb1ba0e98ff17d
Titus Brown <t@titus-browns-macbook.local>
2009-08-12 21:37:31 -0400

    added optional saving of db to disk

--- willow/db.py
+++ willow/db.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker
 _engine = None
 _Session = None
 
-def make_engine():
+def make_engine(uri):
     global _engine, _Session
 
     if _engine is None:
-        _engine = create_engine('sqlite:///:memory:', echo=True)
+        _engine = create_engine(uri, echo=True)
         _Session = sessionmaker()
         _Session.configure(bind=_engine)
 
@@ -17,14 +17,18 @@ def make_engine():
 def get_session():
     return _Session()
 
-def create():
+def create(uri='sqlite:///:memory:'):
     from . import bookmarks
 
-    engine, _ = make_engine()
+    engine, _ = make_engine(uri)
     metadata = bookmarks.Bookmark.metadata
 
     metadata.create_all(engine)
 
+    ## @CTB
+    from bookmarks import Bookmark
     session = get_session()
-    bookmarks.create_a_bunch(session)
-    session.commit()
+    q = session.query(Bookmark).first()
+    if not q:
+        bookmarks.create_a_bunch(session)
+        session.commit()
